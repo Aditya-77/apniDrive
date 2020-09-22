c3be13b6b544e327845fcd91c5ea5d27b22ae1a7
Aditya Shrivastava <18643014+Aditya-77@users.noreply.github.com>
2020-09-22 13:38:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -284,21 +284,16 @@ function file_code(path){
 
 // File display: Video |mp4|webm|avi|
 function file_video(path){
-	var url = window.location.origin + path;
-	
-	/**
-	<div class="mdui-container-fluid">
-		<div class="player">
-     		<iframe class="player_iframe" src="https://drive.google.com/file/d/${id}/preview" webkitallowfullscreen mozallowfullscreen allowfullscreen></webview>
-		</div>
-	</div>
-	**/
-	
+	var url = window.location.origin + path;	
 	var content = `
 <div class="mdui-container-fluid">
-     <div class="mdui-video-fluid mdui-center">
-     		<iframe src="https://drive.google.com/file/d/${id}/preview" webkitallowfullscreen mozallowfullscreen allowfullscreen></webview>
+     <div class="mdui-container-fluid">
+	<br>		
+	<div class="player">
+     		<iframe class="player_iframe" src="https://drive.google.com/file/d/${id}/preview" webkitallowfullscreen mozallowfullscreen allowfullscreen></webview>
 	</div>
+	</br>
+     </div>
 </div>
 <a href="${url}" class="mdui-fab mdui-fab-fixed mdui-ripple mdui-color-theme-accent"><i class="mdui-icon material-icons">file_download</i></a>
 	`;
